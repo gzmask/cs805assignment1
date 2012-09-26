4e1f6b564fd0af258155d80f3e61d8336d279f32
gzmask <gzmask@gmail.com>
2012-09-26 16:52:38 -0600

    types are all working nicely

--- src/get_uvn.cpp
+++ src/get_uvn.cpp
@@ -2,6 +2,6 @@
 
 UVN get_uvn(Point V1, Point V2) {
   UVN result_uvn;
-
+  result_uvn = {V1, V1, V1};
   return result_uvn;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <typeinfo>//debugging only
 #include "get_uvn.h"
 
 int main () {
   Point V1;
-  Point V2;
-  UVN uvn = get_uvn(V1, V2);
+  decltype(V1) V2;//c11: V2 is of same type of V1
+
+  V1 = {1,2,3};
+  V1 = {3,2,1};
+
+  auto uvn = get_uvn(V1, V2);//c11: compiler will replace 'auto' with the right type
+
+  for (auto point : uvn) {//c11:for each point in uvn
+    //std::cout<<typeid(point).name()<<std::endl;
+    for (auto num : point) {//c11:for each number in point
+      std::cout<<num<<std::endl;
+    }
+  }
 
   return 0;
 }
