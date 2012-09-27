dc1215b90953d2429dd587db363e849c5153ff92
Shulang Lei <gzmask@Shulangs-MacBook-Air.local>
2012-09-26 22:10:33 -0600

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@
 
 int main () {
   Point V1;
-  decltype(V1) V2;//c11: V2 is of same type of V1
+  decltype(V1) V2;// V2 is of same type of V1
 
-  V1 = {0,0,1};
+  V1 = {0,0,1000};
   V2 = {0,1,1};
 
-  auto uvn = get_uvn(V1, V2);//c11: compiler will replace 'auto' with the right type
+  auto uvn = get_uvn(V1, V2);// compiler will replace 'auto' with the right type
 
-  for (auto point : uvn) {//c11:for each point in uvn
-    for (auto num : point) {//c11:for each number in point
+  for (auto point : uvn) {//for each point in uvn
+    for (auto num : point) {//for each number in point
       std::cout<<num<<',';
     }
     std::cout<<std::endl;
