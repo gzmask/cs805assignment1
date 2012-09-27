89f151ff282a101a6756d83d8fae5a233c0265f7
Shulang Lei <gzmask@Shulangs-MacBook-Air.local>
2012-09-26 21:38:48 -0600

    code refactored

--- src/util.cpp
+++ src/util.cpp
@@ -1,34 +1,34 @@
 #include "util.h"
 #include <math.h>
 
+//get u,v,n from two non-collinear vectors
 UVN get_uvn(Point V1, Point V2) {
-
   //get n, which is just normalized V1
-  Point n = { V1[0]/get_length(V1), 
-              V1[1]/get_length(V1), 
-              V1[2]/get_length(V1) }; 
-
+  Point n = normalize(V1); 
 
   //get u, which is normalized V2 x V1
-  Point u_ = { V2[1]*V1[2] - V2[2]*V1[1],
-              V2[2]*V1[0] - V2[0]*V1[2],
-              V2[0]*V1[1] - V2[1]*V1[0]};
-  Point u = { (V2[1]*V1[2] - V2[2]*V1[1])/get_length(u_),
-              (V2[2]*V1[0] - V2[0]*V1[2])/get_length(u_),
-              (V2[0]*V1[1] - V2[1]*V1[0])/get_length(u_)};
+  Point u = normalize(cross_product(V2, V1));
 
-  //get v, which is n x u
-  Point v_ = { n[1]*u[2] - n[2]*u[1],
-              n[2]*u[0] - n[0]*u[2],
-              n[0]*u[1] - n[1]*u[0]};
-  Point v = { (n[1]*u[2] - n[2]*u[1])/get_length(v_),
-              (n[2]*u[0] - n[0]*u[2])/get_length(v_),
-              (n[0]*u[1] - n[1]*u[0])/get_length(v_)};
+  //get v, which is normalized n x u
+  Point v = normalize(cross_product(n, u));
 
-  UVN result_uvn = {u, v, n};
-  return result_uvn;
+  return {u,v,n};
 }
 
-float get_length(Point X) {
-  return abs(sqrt(pow(X[0],2)+pow(X[1],2)+pow(X[2],2)));
+//normalize a point
+Point normalize(Point x) {
+  return { x[0]/get_length(x), 
+              x[1]/get_length(x), 
+              x[2]/get_length(x) }; 
+}
+
+//calculates cross product of two points
+Point cross_product(Point x, Point y) {
+  return { x[1]*y[2] - x[2]*y[1],
+              x[2]*y[0] - x[0]*y[2],
+              x[0]*y[1] - x[1]*y[0]};
+}
+
+float get_length(Point x) {
+  return abs(sqrt(pow(x[0],2)+pow(x[1],2)+pow(x[2],2)));
 }
--- src/util.h
+++ src/util.h
@@ -3,7 +3,8 @@
 #include <tr1/array>
 typedef std::tr1::array<float, 3> Point;
 typedef std::tr1::array<Point, 3> UVN;
- 
 UVN get_uvn(Point V1, Point V2);
 float get_length(Point X);
+Point cross_product(Point x, Point y);
+Point normalize(Point x);
 #endif
